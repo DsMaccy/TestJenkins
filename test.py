1f74c2bd09947d08606568421a49db1360440700
Dylan McNamara <dsmaccy@rocketmail.com>
2021-04-22 19:17:12 -0700

    Adding free test

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ class TestBasicClass(unittest.TestCase):
     def testDoSomething(self):
         obj = BasicClass()
         assert obj.doSomething() == "Hello World"
+    def newTest(self):
+        assert True
 
 if __name__ == "__main__":
     unittest.main()
