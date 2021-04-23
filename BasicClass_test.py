e5c5563b17fb671bbdc6d4e97443ee3bc2390172
Dylan McNamara <dsmaccy@rocketmail.com>
2021-04-22 19:38:16 -0700

    Switching over to use pytest

---
+++ BasicClass_test.py
@@ -0,0 +1,12 @@
+import unittest
+from .BasicClass import BasicClass
+
+def test_DoSomething():
+    obj = BasicClass()
+    assert obj.doSomething() == "Hello World"
+def test_success():
+    obj = BasicClass()
+    assert True
+def test_fail():
+    obj = BasicClass()
+    assert False
